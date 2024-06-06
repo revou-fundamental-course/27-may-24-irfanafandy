220dbcd286a36588a12b38ca7bf9432618acf9e1
Irpan <irfan.afandy.jaya@gmail.com>
2024-06-06 17:06:34 +0900

    sesion1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // Ini JavaScript
+
+function validateForm() {
+    const nama = document.forms['message-form']['nama'].value
+
+    if (nama == '') {
+        document.getElementById("error-name").innerHTML = "Tidak boleh kosong!"
+
+        return false
+    }
+
+    document.getElementById("form-rint").innerHTML = form-print;
+    document.getElementById("error-name").innerHTML = "";
+
+        return false;
+}
