a20bdc70ea51158897722e3e08d0112e6b878a8a
Irpan <irfan.afandy.jaya@gmail.com>
2024-06-06 23:33:54 +0900

    color_and_form

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 // Ini JavaScript
 
-function validateForm() {
-    const nama = document.forms['message-form']['nama'].value
 
-    if (nama == '') {
+function validateForm() {
+    const name = document.forms['message-form']['full-name'].value
+    const birthDate = document.forms['message-form']['birth-date'].value
+    const gender = document.forms['message-form']['gender'].value
+    const message = document.forms['message-form']['message'].value
+    
+
+    if (name == "" || birthDate== "" || gender == "" || message == "") {
+        
         document.getElementById("error-name").innerHTML = "Tidak boleh kosong!"
 
-        return false
+            return false
     }
 
-    document.getElementById("form-rint").innerHTML = form-print;
-    document.getElementById("error-name").innerHTML = "";
+    //setSenderUI(name, birthDate, gender, message);
+        setName(name, name, birthDate, gender, message);
 
-        return false;
+            return false;
+}
+
+function setName(name, name, birthDate, gender, message) {
+    document.getElementById("sender-name").innerHTML = name;
+    document.getElementById("sender-hai").innerHTML = name;
+    document.getElementById("sender-birth").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-message").innerHTML = message;
+    document.getElementById("error-name").innerHTML = "";
 }
